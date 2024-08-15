bcb5cbc2af016032002fe4eaea528bb26415e379
abhishekbadole1 <abhishekb@blockshards.in>
2024-08-15 11:48:03 +0530

    UI: SignUp UI created

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.css";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 export default function SignUp() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
-    <div>SignUp</div>
+    <div className={styles.loginPageContainer}>
+
+      <div className={styles.login}>
+        <h3>Sign Up</h3>
+
+        <p>Please enter details to register.</p>
+
+        <input type="text" className={styles.inputField} placeholder="Enter username" />
+
+        <input type="email" className={styles.inputField} placeholder="Enter email" />
+
+        <div className={styles.passwordContainer}>
+          <input
+            type={passwordVisible ? "text" : "password"}
+            className={styles.inputField}
+            placeholder="Enter password"
+          />
+          <div className={styles.eyeIcon} onClick={togglePasswordVisibility}>
+            {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+          </div>
+        </div>
+
+        <div className={styles.passwordContainer}>
+          <input
+            type={passwordVisible ? "text" : "password"}
+            className={styles.inputField}
+            placeholder="Enter confirm password"
+          />
+          <div className={styles.eyeIcon} onClick={togglePasswordVisibility}>
+            {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+          </div>
+        </div>
+
+        <button>Sign Up</button>
+
+      </div>
+
+    </div>
   )
 }
